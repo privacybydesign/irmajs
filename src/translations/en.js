d428ba161262d8a1a83d764d8d79ea00395ef5f3
Ivar Derksen <ivarderksen@hotmail.nl>
2019-08-30 11:26:59 +0200

    IRMA logo updated + texts improved

--- src/translations/en.js
+++ src/translations/en.js
@@ -7,15 +7,15 @@ export default {
     FollowInstructions: 'Please follow the instructions in your IRMA app',
   },
   Sign: {
-    Title: 'signing',
-    Body: 'A website requests that you sign a message using some IRMA attributes. Please scan the QR code with your IRMA app.',
+    Title: 'signature request',
+    Body: 'A website requests that you sign a message using some of your IRMA attributes. Please scan the QR code with your IRMA app.',
   },
   Verify: {
-    Title: 'showing attribute(s)',
-    Body: 'A website requests that you disclose some IRMA attributes. Please scan the QR code with your IRMA app.',
+    Title: 'attribute request',
+    Body: 'A website requests that you disclose some of your IRMA attributes. Please scan the QR code with your IRMA app.',
   },
   Issue: {
-    Title: 'issuing attribute(s)',
-    Body: 'A website wants to issue some IRMA attributes to you. Please scan the QR code with your IRMA app.',
+    Title: 'attribute issuance',
+    Body: 'A website wishes to issue IRMA attributes to you. Please scan the QR code with your IRMA app.',
   },
 };
--- src/translations/nl.js
+++ src/translations/nl.js
@@ -7,15 +7,15 @@ export default {
     FollowInstructions: 'Volg de instructies in uw IRMA app.',
   },
   Sign: {
-    Title: 'ondertekenen',
-    Body: 'Een website vraagt u een bericht te ondertekenen met enkele IRMA attributen. Scan de QR code met uw IRMA app.',
+    Title: 'verzoek om te tekenen',
+    Body: 'Een website vraagt u een bericht te ondertekenen met enkele van uw IRMA attributen. Scan de QR code met uw IRMA app.',
   },
   Verify: {
-    Title: 'attributen tonen',
-    Body: 'Een website vraagt u enkele IRMA attributen te tonen. Scan de QR code met uw IRMA app.',
+    Title: 'verzoek om attributen',
+    Body: 'Een website vraagt u enkele van uw IRMA attributen te tonen. Scan de QR code met uw IRMA app.',
   },
   Issue: {
-    Title: 'attributen uitgeven',
+    Title: 'attributenuitgifte',
     Body: 'Een website wil u enkele IRMA attributen geven. Scan de QR code met uw IRMA app.',
   },
 };
